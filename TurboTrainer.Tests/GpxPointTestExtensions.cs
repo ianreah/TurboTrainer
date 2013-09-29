075a7e559c52900817fd486ce1f87c072ce01a34
Ian Reah <ian.reah@gmail.com>
2013-09-29 12:08:25 +0100

    Refactoring tests...so I can re-use AssertGpxPoint in other tests

---
+++ TurboTrainer.Tests/GpxPointTestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Globalization;
+using TurboTrainer.Core;
+
+namespace TurboTrainer.Tests
+{
+    public static class GpxPointTestExtensions
+    {
+        public static void AssertPoint(this GpxPoint point, double expectedLatitude, double expectedLongitude, double expectedElevation, string expectedTime)
+        {
+            Assert.That(point.Latitude, Is.EqualTo(expectedLatitude));
+            Assert.That(point.Longitude, Is.EqualTo(expectedLongitude));
+            Assert.That(point.Elevation, Is.EqualTo(expectedElevation));
+            Assert.That(point.Time, Is.EqualTo(DateTime.Parse(expectedTime, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal)));
+        }
+    }
+}
--- TurboTrainer.Tests/GpxReaderTests.cs
+++ TurboTrainer.Tests/GpxReaderTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -20,18 +20,10 @@ namespace TurboTrainer.Tests
 
 				Assert.That(reader.Points.Count(), Is.EqualTo(3));
 				
-				AssertGpxPoint(reader.Points.ElementAt(0), 47.644548, -122.326897, 4.46, "2009-10-17T18:37:26Z");
-				AssertGpxPoint(reader.Points.ElementAt(1), 47.644548, -122.326897, 4.94, "2009-10-17T18:37:31Z");
-				AssertGpxPoint(reader.Points.ElementAt(2), 47.644548, -122.326897, 6.87, "2009-10-17T18:37:34Z");
+				reader.Points.ElementAt(0).AssertPoint(47.644548, -122.326897, 4.46, "2009-10-17T18:37:26Z");
+				reader.Points.ElementAt(1).AssertPoint(47.644548, -122.326897, 4.94, "2009-10-17T18:37:31Z");
+				reader.Points.ElementAt(2).AssertPoint(47.644548, -122.326897, 6.87, "2009-10-17T18:37:34Z");
 			}
-		}
-
-		private static void AssertGpxPoint(GpxPoint gpxPoint, double expectedLatitude, double expectedLongitude, double expectedElevation, string expectedTime)
-		{
-			Assert.That(gpxPoint.Latitude, Is.EqualTo(expectedLatitude));
-			Assert.That(gpxPoint.Longitude, Is.EqualTo(expectedLongitude));
-			Assert.That(gpxPoint.Elevation, Is.EqualTo(expectedElevation));
-			Assert.That(gpxPoint.Time, Is.EqualTo(DateTime.Parse(expectedTime, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal)));
 		}
 	}
 }
