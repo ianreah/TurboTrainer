5fad47dd9cea28392c6d261ee74fb2fb0659a340
Ian Reah <ian.reah@gmail.com>
2013-09-29 20:38:24 +0100

    Hooking up some _very_ basic UI...just so I can see something running

---
+++ TurboTrainer.Wpf/FileChooserUi.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using TurboTrainer.Core;
+
+namespace TurboTrainer.Wpf
+{
+    public class FileChooserUi : IFileChooserUi
+    {
+        public Stream ChooseFile()
+        {
+            var dialog = new OpenFileDialog { Filter = "GPX Files (*.gpx)|*.gpx|All files (*.*)|*.*" };
+            if (dialog.ShowDialog() == true)
+            {
+                return File.OpenRead(dialog.FileName);
+            }
+
+            return Stream.Null;
+        }
+    }
+}
