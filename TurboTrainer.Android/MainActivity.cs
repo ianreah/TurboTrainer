320e03829954343bc1a457338b16e931a9676cce
Ian Reah <ian.reah@nonlinear.com>
2013-10-15 13:24:50 +0100

    First stab at hooking up the MainViewModel - MainActivity bindings

--- TurboTrainer.Android/MainActivity.cs
+++ TurboTrainer.Android/MainActivity.cs
@@ -1,16 +1,39 @@
 using Android.App;
 using Android.OS;
+using TurboTrainer.Core;
+using ReactiveUI;
+using Android.Widget;
 
 namespace TurboTrainer
 {
 	[Activity (Label = "TurboTrainer", MainLauncher = true)]
-	public class MainActivity : Activity
+	public class MainActivity : Activity, IViewFor<MainViewModel>
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 			SetContentView (Resource.Layout.Main);
+
+			ViewModel = new MainViewModel(RxApp.TaskpoolScheduler, null /* TODO */);
+
+			GradientText = FindViewById<TextView>(Resource.Id.gradientText);
+			this.OneWayBind(ViewModel, vm => vm.CurrentPoint.Elevation, v => v.GradientText.Text);
+
+			LoadGpxButton = FindViewById<Button>(Resource.Id.loadGpxButton);
+			//this.BindCommand(ViewModel, vm => vm.LoadGpxDataCommand, v => v.LoadGpxButton); <-- this is how I want to do it...work out why this isn't working
+			LoadGpxButton.Click += (sender, e) => ViewModel.LoadGpxDataCommand.Execute(null); // Just do it with a click handler for now!
 		}
+
+		public MainViewModel ViewModel { get; set; }
+
+		object IViewFor.ViewModel
+		{
+			get { return ViewModel; }
+			set { ViewModel = (MainViewModel)value; }
+		}
+		
+		public TextView GradientText { get; private set; }
+		public Button LoadGpxButton { get; private set; }
 	}
 }
 
